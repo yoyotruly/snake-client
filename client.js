e30d70a60003401fa695345349192cecb6bc9d29
Yoyo Yang <yoyo.truly@gmail.com>
2022-04-16 21:40:11 -0400

    Refactor connect function to its own module

---
+++ client.js
@@ -0,0 +1,18 @@
+const net = require("net");
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541
+  });
+
+  conn.setEncoding("utf8");
+
+  conn.on("data", data => {
+    console.log(`Server says: ${data}`);
+  });
+
+  return conn;
+};
+
+module.exports = connect;
