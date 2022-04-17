57137a8952b98cc3e3979b5d6794b5f08db24b91
Yoyo Yang <yoyo.truly@gmail.com>
2022-04-16 21:32:31 -0400

    Add starter play code to establish connection to the snake server

---
+++ play.js
@@ -0,0 +1,15 @@
+const net = require("net");
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541
+  });
+
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+console.log("Connecting...");
+connect();
