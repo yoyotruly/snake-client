86b9247699d4ddfe499a6e09079476bab85b1455
Yoyo Yang <yoyo.truly@gmail.com>
2022-04-20 19:13:27 -0400

    Refactor code to require input module

--- play.js
+++ play.js
@@ -1,25 +1,7 @@
 const { connect } = require("./client");
-
-const handleUserInput = function(key) {
-  if (key === "\u0003") {
-    process.exit();
-  }
-};
-
-const setupInput = function() {
-  const stdin = process.stdin;
-
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
-
-setupInput();
-
+const { setupInput } = require("./input");
 
 console.log("Connecting...");
 connect();
+
+setupInput();
