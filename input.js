09e3b5d18806e45e184db128bb29bdc36931ce6e
Yoyo Yang <yoyo.truly@gmail.com>
2022-04-20 19:38:35 -0400

    Add handling of wasd for directions

--- input.js
+++ input.js
@@ -1,16 +1,32 @@
+let connection;
+
 const handleUserInput = function(key) {
-  if (key === "\u0003") {
+  switch (key) {
+  case "w":
+    connection.write("Move: up");
+    break;
+  case "a":
+    connection.write("Move: left");
+    break;
+  case "s":
+    connection.write("Move: down");
+    break;
+  case "d":
+    connection.write("Move: right");
+    break;
+  case "\u0003":
     process.exit();
   }
 };
 
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
+
   const stdin = process.stdin;
 
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-
   stdin.on("data", handleUserInput);
 
   return stdin;
