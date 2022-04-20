0fce8a6d02a84ab1cd53f9c19d57864cab098509
Yoyo Yang <yoyo.truly@gmail.com>
2022-04-20 19:10:09 -0400

    Add function to handle user input

--- play.js
+++ play.js
@@ -1,5 +1,25 @@
 const { connect } = require("./client");
 
+const handleUserInput = function(key) {
+  if (key === "\u0003") {
+    process.exit();
+  }
+};
+
+const setupInput = function() {
+  const stdin = process.stdin;
+
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+setupInput();
+
 
 console.log("Connecting...");
 connect();
